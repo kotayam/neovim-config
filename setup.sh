3548bd09e5709476e5c27540db66472f85cca0a6
kotayam <ykomako4.9@gmail.com>
2025-07-31 19:46:45 -0400

    update setup sh lua dir linking

--- setup.sh
+++ setup.sh
@@ -14,8 +14,10 @@ echo "Linking lazy-lock.json"
 ln -sf "$(pwd)/lazy-lock.json" "$CONFIG_DIR/lazy-lock.json"
 
 echo "Linking lua directory"
+# Remove if exists
+if [ -e "$CONFIG_DIR/lua" ] || [ -L "$CONFIG_DIR/lua" ]; then
+    rm -rf "$CONFIG_DIR/lua"
+fi
 ln -sf "$(pwd)/lua" "$CONFIG_DIR/lua"
 
 echo "Completed Successfully!"
-
-
