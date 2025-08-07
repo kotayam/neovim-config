5b239de1703c5cced7f081e88388f593ba06046a
kotayam <ykomako4.9@gmail.com>
2025-08-07 18:50:44 -0400

    remove buffer mapping

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,71 +1,81 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-    },
-    config = function()
-        local telescope = require("telescope")
-        telescope.setup {
-            pickers = {
-                find_files = {
-                    theme = "ivy"
-                },
-                git_files = {
-                    theme = "ivy"
-                },
-                buffers = {
-                    theme = "ivy"
-                },
-                live_grep = {
-                    theme = "ivy"
-                },
-                grep_string = {
-                    theme = "ivy"
-                }
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<c-d>"] = actions.delete_buffer,
+					},
+					n = {
+						["dd"] = actions.delete_buffer,
+					},
+				},
+			},
+			pickers = {
+				find_files = {
+					theme = "ivy",
+				},
+				git_files = {
+					theme = "ivy",
+				},
+				buffers = {
+					theme = "ivy",
+				},
+				live_grep = {
+					theme = "ivy",
+				},
+				grep_string = {
+					theme = "ivy",
+				},
+			},
+			extensions = {
+				fzf = {},
+			},
+		})
+		-- load fzf
+		telescope.load_extension("fzf")
 
-            },
-            extensions = {
-                fzf = {}
-            }
-        }
-        -- load fzf
-        telescope.load_extension('fzf')
+		local builtin = require("telescope.builtin")
+		-- find directory files
+		vim.keymap.set("n", "<leader>fd", builtin.find_files)
 
-        local builtin = require('telescope.builtin')
-        -- find directory files
-        vim.keymap.set("n", "<leader>fd", builtin.find_files)
+		-- find git files
+		vim.keymap.set("n", "<leader>fg", builtin.git_files)
 
-        -- find git files
-        vim.keymap.set("n", "<leader>fg", builtin.git_files)
+		-- find buffers
+		vim.keymap.set("n", "<leader>fb", builtin.buffers)
 
-        -- find buffers
-        vim.keymap.set("n", "<leader>fb", builtin.buffers)
+		-- grep files
+		-- vim.keymap.set("n", "<leader>lg", builtin.live_grep)
 
-        -- grep files
-        -- vim.keymap.set("n", "<leader>lg", builtin.live_grep)
+		-- edit neovim
+		vim.keymap.set("n", "<leader>en", function()
+			builtin.find_files({
+				cwd = vim.fn.stdpath("config"),
+			})
+		end)
 
-        -- edit neovim
-        vim.keymap.set("n", "<leader>en", function()
-            builtin.find_files {
-                cwd = vim.fn.stdpath("config")
-            }
-        end)
+		-- edit pacakges
+		vim.keymap.set("n", "<leader>ep", function()
+			builtin.find_files({
+				cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
+			})
+		end)
 
-        -- edit pacakges
-        vim.keymap.set("n", "<leader>ep", function()
-            builtin.find_files {
-                cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
-            }
-        end)
+		-- vim.keymap.set("n", "<leader>fg", function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
 
-        -- vim.keymap.set("n", "<leader>fg", function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
+		-- find help
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags)
 
-        -- find help
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags)
-
-        -- custom function for live multi grep
-        require("config.telescope.multigrep").setup()
-    end
+		-- custom function for live multi grep
+		require("config.telescope.multigrep").setup()
+	end,
 }
