f7a73659aa66651a145d84855964816d8c83f92f
kotayam <ykomako4.9@gmail.com>
2025-08-21 14:23:34 -0400

    format

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,17 +1,33 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            ensure_installed =
-            { "c", "lua", "javascript", "typescript", "vim", "tsx",
-                "json", "css", "html", "yaml", "prisma", "markdown",
-                "markdown_inline", "vimdoc", "go", "templ", "make", "bash" },
-            sync_install = false,
-            auto_install = true,
-            ignore_install = {},
-            highlight = { enable = true },
-        })
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"javascript",
+				"typescript",
+				"vim",
+				"tsx",
+				"json",
+				"css",
+				"html",
+				"yaml",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"vimdoc",
+				"go",
+				"templ",
+				"make",
+				"bash",
+			},
+			sync_install = false,
+			auto_install = true,
+			ignore_install = {},
+			highlight = { enable = true },
+		})
+	end,
 }
